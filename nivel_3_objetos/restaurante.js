6a2507ec243562105fa536a9f3eda1c479da110b
daniela <d27090342@gmail.com>
2026-03-03 12:54:14 -0500

    creando objeto cliente

--- nivel_3_objetos/restaurante.js
+++ nivel_3_objetos/restaurante.js
@@ -7,33 +7,3 @@ let cliente = {
 };
 
 
-//Creamos las funciones
-
-function ordenarComida(cliente) {
-    console.log(cliente.nombre + " está ordenando comida ");
-
-    cliente.hambre = cliente.hambre - 30;
-}
-
-function pagar(cliente) {
-    console.log(cliente.nombre + " está pagando");
-
-    cliente.dinero = cliente.dinero - 20;
-}
-
-function mostrarEstado(cliente) {
-    console.log("---- ESTADO DEL CLIENTE ----");
-    console.log("Nombre:", cliente.nombre);
-    console.log("Dinero:", cliente.dinero);
-    console.log("Hambre:", cliente.hambre);
-}
-
-
-// SIMULACIÓN
-
-mostrarEstado(cliente);
-
-ordenarComida(cliente);
-pagar(cliente);
-
-mostrarEstado(cliente);
