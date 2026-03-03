c5760c144d33934b16c25312dddc812b43dd2b45
carlos <carlos.campusano15042007@gmail.com>
2026-03-02 21:53:24 -0500

    creando heroe1.js

---
+++ nivel_1_variables/heroe1.js
@@ -0,0 +1,12 @@
+let nombre = "superman";
+let edad = 30;
+let poder = "super fuerza";
+let energia = 100;
+let villano = 'batman';
+
+
+console.log("nombre: " + nombre);
+console.log("edad: " + edad);
+console.log("poder: " + poder);
+console.log("energia: " + energia);
+console.log("villano: " + villano);
