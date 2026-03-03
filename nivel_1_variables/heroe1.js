abae77ec6751473827c42fefb30416664e31c2e5
carlos <carlos.campusano15042007@gmail.com>
2026-03-02 22:01:58 -0500

    agregando historia heroe1.js

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -10,3 +10,6 @@ console.log("edad: " + edad);
 console.log("poder: " + poder);
 console.log("energia: " + energia);
 console.log("villano: " + villano);
+
+let historia = "superman es un heroe que lucha contra el mal,tiene 30 años, su poder es la super fuerza, se enfrenta a su enemigo llamado badman";
+console.log("historia: " + historia);
