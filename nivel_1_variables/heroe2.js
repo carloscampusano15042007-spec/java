59b135be6b1360f7df4bddb7f3d94ea866d228d5
daniela <d27090342@gmail.com>
2026-03-02 21:53:24 -0500

    creando heroe2.js

---
+++ nivel_1_variables/heroe2.js
@@ -0,0 +1,13 @@
+// Crear variables de nuestro heroe2
+
+let nombre = "Spiderman";
+let edad = 25;
+let poder = "Fuerza sobrehumana";
+let energia = 100;
+let villano = "Venom";
+
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Poder: " + poder);
+console.log("Energia:" + energia);
+console.log("Villano: " + villano);
