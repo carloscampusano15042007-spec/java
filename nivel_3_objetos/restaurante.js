fda5681705aadd73d796174c4a41a730d03f1edf
carlos <carlos.campusano15042007@gmail.com>
2026-03-03 13:25:06 -0500

    funcion estadocliente

--- nivel_3_objetos/restaurante.js
+++ nivel_3_objetos/restaurante.js
@@ -7,12 +7,32 @@ let cliente = {
 
 //funcion ordenar comida(cliente)
 function ordenarComida(cliente) {
-    console.log(cliente.nombre + "esta ordenando comida");
+    console.log(cliente.nombre + " esta ordenando comida");
     cliente.hambre = cliente.hambre - 20;
 
 }
 
 function pagar(cliente) {
-    console.log(cliente.nombre + "esta pagando la comida");
+    console.log(cliente.nombre + " esta pagando la comida");
     cliente.dinero = cliente.dinero - 10;
 }
+
+function estado(cliente) {
+    console.log(cliente.nombre + "");
+    cliente.hambre = cliente.hambre - 20;
+}
+
+function estadoCliente(cliente) {
+    console.log("  ESTADO DEL CLIENTE ");
+    console.log("nombre: " + cliente.nombre);
+    console.log("dinero: " + cliente.dinero);
+    console.log("hambre: " + cliente.hambre);
+    console.log(" GRACIAS POR SU COMPRA");
+}
+
+
+//simulacion
+
+ordenarComida(cliente);
+pagar(cliente);
+estadoCliente(cliente);
