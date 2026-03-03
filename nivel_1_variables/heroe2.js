d94fb94052ce59f996376bfe697be48381c35cd3
daniela <d27090342@gmail.com>
2026-03-02 22:01:58 -0500

    agregando historia heroe2.js

--- nivel_1_variables/heroe2.js
+++ nivel_1_variables/heroe2.js
@@ -6,8 +6,15 @@ let poder = "Fuerza sobrehumana";
 let energia = 100;
 let villano = "Venom";
 
+
 console.log("Nombre: " + nombre);
 console.log("Edad: " + edad);
 console.log("Poder: " + poder);
 console.log("Energia:" + energia);
 console.log("Villano: " + villano);
+
+
+// Creando historia de nuestro heroe2
+let historia = "Superman tiene 25 años y esta defendiendo la ciudad de Nueva York de Venom";
+console.log(historia);
+
