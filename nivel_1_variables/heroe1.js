e2bd18e0ead377f469cf6a5e41512eb017e473bb
daniela <d27090342@gmail.com>
2026-03-03 11:12:37 -0500

    Conflicto resuelto en heroe1.js

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -2,32 +2,26 @@ let nombre = "superman";
 let edad = 30;
 let poder = "super fuerza";
 let energia = 100;
-let villano = 'batman';
-
+let villano = "batman";
 
 console.log("nombre: " + nombre);
-<<<<<<< HEAD
-=======
 console.log("edad: " + edad);
 console.log("poder: " + poder);
 console.log("energia: " + energia);
 console.log("villano: " + villano);
->>>>>>> daniela
 
-let historia = "superman es un heroe que lucha contra el mal,tiene 30 años, su poder es la super fuerza, se enfrenta a su enemigo llamado badman";
+let historia = "Superman es un héroe que lucha contra el mal, tiene 30 años, su poder es la super fuerza, se enfrenta a su enemigo llamado Batman";
 console.log("historia: " + historia);
 
-
-<<<<<<< HEAD
 // Modificando variables
 edad = 18;
 poder = "volar";
 energia = 50;
 villano = "goku";
 
-console.log("edad:" + edad);
-console.log("poder:" + poder);
-console.log("Energia:" + energia);
-console.log("Villano:" + villano);
-=======
->>>>>>> daniela
+console.log("edad: " + edad);
+console.log("poder: " + poder);
+console.log("energia: " + energia);
+console.log("villano: " + villano);
+
+
