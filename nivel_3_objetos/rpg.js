59125434a1533aa0c29580014dc8946b989b4659
daniela <d27090342@gmail.com>
2026-03-05 09:26:23 -0500

    crear funcion subirNivel

--- nivel_3_objetos/rpg.js
+++ nivel_3_objetos/rpg.js
@@ -8,18 +8,25 @@ let jugador = {
 
 }
 
-function atacar(jugador) {
+function atacar() {
     console.log(jugador.nombre + " esta atacando");
     jugador.vida = jugador.vida - 10;
     console.log("vida del jugador: " + jugador.vida);
 }
 
-function curar(jugador) {
+function curar() {
     console.log("Curando al jugador: " + jugador.vida);
     jugador.vida = jugador.vida + 10;
     console.log("Curando al jugador: " + jugador.vida);
 
 }
+
+function subirNivel() {
+    console.log("Subiendo nivel al jugador: " + jugador.nivel);
+    jugador.nivel = jugador.nivel + 1;
+    console.log("Subiendo nivel al jugador: " + jugador.nivel);
+}
+
 
 
 
